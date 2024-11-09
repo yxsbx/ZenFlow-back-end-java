9dc1ccd26efb4c28c7d06e665b1206e4aeb210e7
yxsbx <yas.barceloss@gmail.com>
2024-11-09 13:51:56 -0300

    Adding UserDetailsServiceAutoConfiguration

--- src/main/java/com/zenflow/zenflow_back_end_java/ZenflowBackEndJavaApplication.java
+++ src/main/java/com/zenflow/zenflow_back_end_java/ZenflowBackEndJavaApplication.java
@@ -2,11 +2,12 @@ package com.zenflow.zenflow_back_end_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ZenflowBackEndJavaApplication {
 
     public static void main(String[] args) {
