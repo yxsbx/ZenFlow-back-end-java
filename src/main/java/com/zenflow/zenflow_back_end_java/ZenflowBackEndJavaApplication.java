1bb46db68544bab07a022240a06c3921dc7c34de
yxsbx <yas.barceloss@gmail.com>
2024-11-09 13:02:38 -0300

    Fixing cors config again

--- src/main/java/com/zenflow/zenflow_back_end_java/ZenflowBackEndJavaApplication.java
+++ src/main/java/com/zenflow/zenflow_back_end_java/ZenflowBackEndJavaApplication.java
@@ -2,12 +2,28 @@ package com.zenflow.zenflow_back_end_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ZenflowBackEndJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZenflowBackEndJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZenflowBackEndJavaApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "https://zen-flow-front-end-angular.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
