c32b323158aecc1e8b26c6f47fd91bc462df25b8
yxsbx <yas.barceloss@gmail.com>
2024-11-12 12:46:44 -0300

    Fixing firebase .env

--- src/main/java/com/zenflow/zenflow_back_end_java/config/FirebaseConfig.java
+++ src/main/java/com/zenflow/zenflow_back_end_java/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.zenflow.zenflow_back_end_java.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -12,10 +13,12 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.credentials.path}")
+    private String firebaseCredentialsPath;
+
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase-adminsdk.json");
+        FileInputStream serviceAccount = new FileInputStream(firebaseCredentialsPath);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
